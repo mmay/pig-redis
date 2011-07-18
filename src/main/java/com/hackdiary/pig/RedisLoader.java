8fe8e7d8fd04cd563b1e716868bd3686c7fc194d
Michael May <curiax8@gmail.com>
2011-07-18 17:13:41 -0500

    creating maven module over existing source

---
+++ src/main/java/com/hackdiary/pig/RedisLoader.java
@@ -0,0 +1,11 @@
+package com.hackdiary.pig;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Michael
+ * Date: 6/29/11
+ * Time: 2:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RedisLoader {
+}
